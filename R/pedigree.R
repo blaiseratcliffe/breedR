76a94d63b852bc131e5e6033284a72856330b06f
Blaise Ratcliffe <b.ratcliffe@gmail.com>
2026-09-23 12:29:27 -0700

    Refuse negative parent codes and codes above the integer range in build_pedigree() (#66, #67)

--- R/pedigree.R
+++ R/pedigree.R
@@ -59,6 +59,26 @@ build_pedigree <- function(x, self = x[[1]], sire = x[[2]], dam = x[[3]], data) 
   if( any(ped$self < 1, na.rm = TRUE) )
     stop("Individual codes must be 1 or greater (0 marks an unknown parent)")
 
+  # A parent code is 0 or NA (unknown parent) or the code of an individual.
+  # A negative code is neither, and as a subscript of the recoding map it
+  # would drop entries instead of looking one up (#66)
+  parents <- c(ped$sire, ped$dam)
+  neg_codes <- unique(parents[which(parents < 0)])
+  if( length(neg_codes) )
+    stop("Parent codes must be 0 or NA (unknown parent), or 1 or greater. ",
+         "Negative parent codes: ", toString(utils::head(sort(neg_codes), 10L)),
+         if( length(neg_codes) > 10L ) ", ...")
+
+  # Codes are stored as integers, and the recoding map is indexed by code (#67)
+  codes <- c(ped$self, parents)
+  big_codes <- unique(codes[which(codes > .Machine$integer.max)])
+  if( length(big_codes) )
+    stop("Pedigree codes must not exceed .Machine$integer.max (",
+         .Machine$integer.max, "). Codes out of range: ",
+         toString(format(utils::head(sort(big_codes), 10L),
+                         scientific = FALSE, trim = TRUE)),
+         if( length(big_codes) > 10L ) ", ...")
+
   # 0 is to be interpreted as unknown parent, not as an individual code
   # recode it as NA
   ped$sire[which(ped$sire == 0)] <- NA
